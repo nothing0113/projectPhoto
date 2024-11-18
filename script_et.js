b3c365753f4737cba58d3b29727e4d48d070f4f5
PSW <tjddhks1725@naver.com>
2024-11-18 11:48:35 +0900

    80% 완성

---
+++ script_et.js
@@ -0,0 +1,38 @@
+// script.js
+let index = 1; // 복제된 첫 번째 이미지를 시작 위치로 설정
+const slides = document.querySelector(".slides");
+const slideImages = document.querySelectorAll(".slides img");
+const slideCount = slideImages.length;
+const slideWidth = 400; // 슬라이드 이미지의 너비
+const intervalTime = 3000; // 슬라이드가 넘어가는 시간 간격 (밀리초)
+
+// 초기 위치 설정 (첫 번째 슬라이드 위치로 이동)
+slides.style.transform = `translateX(-${slideWidth * index}px)`;
+
+function moveToNextSlide() {
+    index++;
+    slides.style.transition = "transform 0.8s ease"; // 이동 속도 조정
+    slides.style.transform = `translateX(-${slideWidth * index}px)`;
+
+    // 마지막 슬라이드에서 첫 슬라이드로 돌아가도록 처리
+    slides.addEventListener("transitionend", () => {
+        if (index === slideCount - 1) {
+            index = 1; // 첫 번째 이미지로 돌아감
+            slides.style.transition = "none"; // 애니메이션을 잠시 비활성화
+            slides.style.transform = `translateX(-${slideWidth * index}px)`; // 첫 번째 슬라이드로 이동
+            setTimeout(() => {
+                slides.style.transition = "transform 0.8s ease"; // 애니메이션 다시 활성화
+            }, 50); // 짧은 지연 시간 후에 애니메이션 활성화
+        }
+    });
+}
+
+
+
+function goToHomepage() {
+    window.location.href = "main.html"; // 이동할 홈페이지 URL
+}
+
+// 일정 시간마다 moveToNextSlide 함수 호출
+setInterval(moveToNextSlide, intervalTime);
+
---
+++ script_mi.js
@@ -0,0 +1,74 @@
+const menuButton = document.getElementById('menu-button');
+const dropdownMenu = document.getElementById('dropdown-menu');
+const imagePaths = [
+    './images/DSCF1395.jpg','./images/DSCF1399.jpg','./images/DSCF1400.jpg',
+    './images/DSCF1411.jpg','./images/DSCF1420.jpg','./images/DSCF1476.jpg',
+    './images/DSCF1772.jpg','./images/DSCF1773.jpg','./images/DSCF1777.jpg',
+    './images/DSCF1778.jpg','./images/DSCF1779.jpg','./images/DSCF1780.jpg',
+    './images/DSCF1781.jpg','./images/DSCF1782.jpg','./images/DSCF1783.jpg',
+    './images/DSCF1784.jpg','./images/DSCF1785.jpg','./images/DSCF1786.jpg',
+    './images/DSCF1787.jpg','./images/DSCF1788.jpg','./images/DSCF1788.jpg',
+    './images/DSCF1789.jpg','./images/DSCF1790.jpg','./images/DSCF1791.jpg',
+    './images/DSCF1792.jpg','./images/DSCF1793.jpg','./images/DSCF1794.jpg',
+    './images/DSCF1795.jpg','./images/DSCF1797.jpg','./images/DSCF1799.jpg',
+    './images/DSCF1800.jpg','./images/DSCF1801.jpg','./images/DSCF1803.jpg',
+    './images/DSCF1804.jpg','./images/DSCF1805.jpg','./images/DSCF1806.jpg',
+    './images/DSCF1807.jpg','./images/DSCF1808.jpg','./images/DSCF1809.jpg',
+    './images/DSCF1810.jpg','./images/DSCF1811.jpg','./images/DSCF1812.jpg',
+    './images/DSCF1813.jpg','./images/DSCF1814.jpg','./images/DSCF1815.jpg',
+    './images/DSCF1819.jpg','./images/DSCF1820.jpg','./images/DSCF1821.jpg',
+    './images/DSCF1822.jpg','./images/DSCF1823.jpg','./images/DSCF1824.jpg',
+    './images/DSCF1825.jpg','./images/DSCF1826.jpg','./images/DSCF1827.jpg',
+    './images/DSCF1828.jpg','./images/DSCF1829.jpg','./images/DSCF1830.jpg',
+    './images/DSCF1831.jpg','./images/DSCF1832.jpg','./images/DSCF1833.jpg',
+    './images/DSCF1834.jpg','./images/DSCF1835.jpg','./images/DSCF1836.jpg',
+    './images/DSCF1837.jpg','./images/DSCF1838.jpg','./images/DSCF1839.jpg',
+    './images/DSCF1840.jpg','./images/DSCF1841.jpg','./images/DSCF1842.jpg',
+    './images/DSCF1843.jpg','./images/DSCF1844.jpg','./images/DSCF1845.jpg',
+    './images/DSCF1846.jpg','./images/DSCF1847.jpg','./images/DSCF1848.jpg',
+    './images/DSCF1849.jpg','./images/DSCF1850.jpg','./images/DSCF1851.jpg',
+    './images/DSCF1852.jpg','./images/DSCF1853.jpg','./images/DSCF1854.jpg',
+    './images/DSCF1855.jpg','./images/DSCF1856.jpg','./images/DSCF1857.jpg',
+    './images/DSCF1858.jpg','./images/DSCF1859.jpg','./images/DSCF1860.jpg'
+];
+
+// 메뉴 버튼 클릭 이벤트 설정
+menuButton.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('hidden'); // 'hidden' 클래스를 추가/제거하여 메뉴 표시/숨김
+});
+
+// 페이지 아무 곳이나 클릭 시 메뉴 닫기
+document.addEventListener('click', (event) => {
+    if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.classList.add('hidden'); // 메뉴 숨김
+    }
+});
+
+window.onload = generateRandomImages;
+
+function generateRandomImages() {
+    const gallery = document.getElementById('gallery');
+    gallery.innerHTML = ''; // 기존 내용을 초기화
+
+    // 랜덤으로 이미지 배열을 섞음
+    const shuffledImages = imagePaths.sort(() => 0.5 - Math.random());
+
+    // 섞인 이미지 배열에서 첫 8개 이미지를 갤러리에 추가
+    shuffledImages.slice(0, 8).forEach((path, index) => {
+        const imageCard = document.createElement('div');
+        imageCard.className = 'p-4';
+
+        imageCard.innerHTML = `
+        <div class="aspect-square overflow-hidden group">
+            <a href="gallery.html">
+                <img src="${path}" loading="lazy" alt="Random Image ${index + 1}" 
+                    class="object-cover w-full h-full group-hover:opacity-75 transition duration-300">
+            </a>
+        </div>
+        <p class="mt-2 text-left font-semibold">전시회 ${index + 1}</p>
+    `;
+    
+
+        gallery.appendChild(imageCard);
+    });
+}
